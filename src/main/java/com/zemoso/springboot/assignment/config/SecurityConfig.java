f3ad03f3e3b791a841a1325f8b428d351a3ae9fb
Vibhuti Narayan Patel <vibhuti.narayan@zemosolabs.com>
2023-07-05 11:31:02 +0530

    test case update 2

--- src/main/java/com/zemoso/springboot/assignment/config/SecurityConfig.java
+++ src/main/java/com/zemoso/springboot/assignment/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.zemoso.springboot.assignment.config;
 
+
 import com.zemoso.springboot.assignment.auth.LogoutHandler;
+import lombok.Generated;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
+@Generated
 public class SecurityConfig {
+
     private final LogoutHandler logoutHandler;
 
     public SecurityConfig(LogoutHandler logoutHandler) {
@@ -22,6 +27,7 @@ public class SecurityConfig {
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .addLogoutHandler(logoutHandler)
-                .and().build();
+                .and()
+                .build();
     }
 }
